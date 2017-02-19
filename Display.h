b94117b4335b7e27b25d331a744a2adfe65d22bd
zerbroesler <zerbroesler@gmx.de>
2017-02-19 10:33:08 +0100

    Adapted buttons to new Mega pins

--- Display.h
+++ Display.h
@@ -6,7 +6,7 @@
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-#define DISPLAYSPEED 500
+#define DISPLAYSPEED 2000
 #define VALUEKNOBS 3
 
 
