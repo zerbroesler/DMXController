c51806fa77e5df7ae360be1f58200e4e1c79f919
zerbroesler <zerbroesler@gmx.de>
2017-03-01 20:50:49 +0100

    findStepState created, but not active

--- Program.h
+++ Program.h
@@ -16,6 +16,7 @@
 
 boolean programRunning = true;
 int programSpeedStep=0;
+unsigned long globalProgramStart=0;
 
 struct values{
   byte r;
@@ -40,21 +41,28 @@ struct program{
   struct programStep steps[MAX_PROGRAM_STEPS];
 };
 
+struct stepState{
+  byte percent;
+  byte stepNumber;
+  boolean keep;
+};
+
 struct programState{
+  unsigned long programDuration;
   unsigned long stepStartMilliseconds;
   boolean keep;
   int currentStep;
 };
 
 struct programState programStates[MAX_PROGRAMS]={
-  {0,true,0},
-  {0,true,0}
+  {0,0,true,0},
+  {0,0,true,0}
 };
 
 struct program programs[MAX_PROGRAMS]={  // Demo program fade between Red and Blue 2&1 Second
     {
       3,        // Steps
-      80,0,0,  //values
+      50,0,0,  //values
       RGB,     // RGB
       5000,     // keep duration before transistion
       4000,     // duration
@@ -70,13 +78,21 @@ struct program programs[MAX_PROGRAMS]={  // Demo program fade between Red and Bl
       FADE_RGB,
       0,
 
-      0,80,0,
+      0,60,0,
       RGB,   
       5000,
       4000,
       1,      
       FADE_RGB,
       0,
+    },{
+      0
+    },{
+      0
+    },{
+      0
+    },{
+      0
     }
 };
 
@@ -84,9 +100,25 @@ bool checkProgramRunning(int number){
   return false;
 };
 
+unsigned long calculateProgramDuration(int programNumber){
+  unsigned long programDuration=0;
+  for(int i=0;i<programs[programNumber].numberOfSteps;i++){
+    programDuration+=programs[programNumber].steps[i].keepInMilliseconds;
+    programDuration+=programs[programNumber].steps[i].durationInMilliseconds;    
+  }
+  return programDuration;
+}
+
+void rememberAllProgramsDurations(){
+  for(int i = 0;i<MAX_PROGRAMS;i++){
+    programStates[i].programDuration = calculateProgramDuration(i);
+  }
+}
+
 void startProgram(){
   int number = 0;
-//  checkProgramRunning(number);
+  rememberAllProgramsDurations();
+  globalProgramStart = millis();
   programStates[number].stepStartMilliseconds=millis();
   programStates[number].keep=true;
   programStates[number].currentStep=0;
@@ -185,9 +217,34 @@ struct RgbColor mixColorsOfSteps(programStep *thisStep, programStep *nextStep,in
 //  }
 //}
 
-void getProgramPosition(unsigned long milliseconds,unsigned long offset,programNumber){
-  // Calculate where we are...
-}
+
+struct stepState findStepState(unsigned long milliseconds,unsigned long offset,byte programNumber){
+  // Calculate where we are... 
+  // milliseconds is the golbal clock
+  // offset is the offset relative to the global start time
+  // Calculates the percentage in the current step and the keep/fade flag
+
+  unsigned long timeWithinProgram = (milliseconds - globalProgramStart) %  programStates[programNumber].programDuration;
+
+  stepState currentStepState;
+  unsigned long programDuration=0;
+  for(int i=0;i<programs[programNumber].numberOfSteps;i++){
+    currentStepState.stepNumber=i;
+    currentStepState.percent = (timeWithinProgram-programDuration)/(programs[programNumber].steps[i].keepInMilliseconds+1);//Avoid div/0
+    programDuration+=programs[programNumber].steps[i].keepInMilliseconds;
+    if(timeWithinProgram < programDuration){
+      currentStepState.keep=true;
+      return currentStepState;
+    }
+    currentStepState.percent = (timeWithinProgram-programDuration)/(programs[programNumber].steps[i].durationInMilliseconds+1);//Avoid div/0
+    currentStepState.percent = timeWithinProgram-programDuration;
+    programDuration+=programs[programNumber].steps[i].durationInMilliseconds;    
+    if(timeWithinProgram < programDuration){
+      currentStepState.keep=false;
+      return currentStepState;
+    }
+  }
+};
 
 void programExecutor(){
   // Todo: Spagetthi coding
